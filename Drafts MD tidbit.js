3d8563c4c9c155a2d6975e851220794eda3d3f10
Roberto Mateu <rmateu@gmail.com>
2019-03-28 21:18:13 -0600

    Create Drafts MD tidbit.js
    1st working version

---
+++ Drafts MD tidbit.js
@@ -0,0 +1,5 @@
+//drafts5://x-callback-url/create?text=Hello%20World
+
+var md = "-%20[" + window.getSelection().toString() + "]" + "%0A%0A" + "[" + window.getSelection().toString() + "]: " + document.URL + " \"" +document.title + "\"" +"%0A" ;
+
+window.location.href = "drafts5://prepend?uuid=981735F6-1304-4735-A679-4EB05333763F&text=" + md

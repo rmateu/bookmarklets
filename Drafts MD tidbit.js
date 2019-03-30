422e4b3252ca5848b512b56296ac9322eb5725c2
Roberto Mateu <rmateu@gmail.com>
2019-03-30 13:15:24 -0600

    Update Drafts MD tidbit.js
    added ink toreference

--- Drafts MD tidbit.js
+++ Drafts MD tidbit.js
@@ -1,4 +1,5 @@
 //drafts5://x-callback-url/create?text=Hello%20World
+// https://reference.getdrafts.com/topics/urls
 
 var md = "-%20[" + window.getSelection().toString() + "]" + "%0A%0A" + "[" + window.getSelection().toString() + "]: " + document.URL + " \"" +document.title + "\"" +"%0A" ;
 

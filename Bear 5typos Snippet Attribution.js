5f25e88e335bd38cd46a7dabb5867858e5404e3b
Roberto Mateu <rmateu@gmail.com>
2019-07-25 18:04:49 -0600

    Create Bear 5typos Snippet Attribution.js
    Snippet 5typost bookmarklet for Bear.app

---
+++ Bear 5typos Snippet Attribution.js
@@ -0,0 +1,23 @@
+// bear://x-callback-url/create?title=My%20Note%20Title&text=First%20line&tags=home,home%2Fgroceries
+// https://bear.app/faq/X-callback-url%20Scheme%20documentation/#add-text
+// Bookmarklet Maker: https://caiorss.github.io/bookmarklet-maker/
+
+var url = document.URL;
+        t = location.host;
+        t = t.replace(/^www./, "");
+	text = window.getSelection().toString();
+	mdRef = '[' + document.title + '][idk]';
+        nTitle = document.title ;
+        mdLink = '[idk]:%20' + url + " \"" +document.title + "\"" +"%0A";
+
+
+window.location.href = 'bear://x-callback-url/create?'
+                        + 'text=%23%23%20' 
+                        +  mdRef
+                        + '%0A%0A'
+                        + '[' + t + '][idk]:' 
+                        + '%0A%0A'
+                        + '>%20' + text
+                        + '%0A%0A'
+                        + mdLink
+                        + '&tags=5typos%2Fsnippet'

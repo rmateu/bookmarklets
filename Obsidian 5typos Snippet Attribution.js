2b25fcf0a4df1f9a035708caad9f01120c5ce8c6
Roberto Mateu <rmateu@aeropost.com>
2022-02-01 11:58:42 -0600

    Create Obsidian 5typos Snippet Attribution.js
    and added ## to heading

---
+++ Obsidian 5typos Snippet Attribution.js
@@ -0,0 +1,26 @@
+// Reference https://help.obsidian.md/Advanced+topics/Using+obsidian+URI
+// Example obsidian://new?vault=main&file=Personal%2FPosts%2FSnippets%2Fsnippet&content=
+// Bookmarklet Maker: https://caiorss.github.io/bookmarklet-maker/
+
+var url = document.URL;
+        t = location.host;
+        t = t.replace(/^www./, "");
+	selectedText = window.getSelection().toString();
+        idRef =  t.substring(0, 4) + Math.floor((Math.random() * 898) + 101);
+	mdRef = '[' + document.title + '][' + idRef + ']'
+        mdLink = '[' + idRef + ']:%20' + url + " \"" +document.title + "\"" +"%0A";
+
+
+
+window.location.href = 'obsidian://new?vault=main'
+                        + '&file=Personal%2FPosts%2FSnippets' 
+                        + '%2Fsnippet' 
+                        + '&content='
+                        +  '%23%23%20' + mdRef
+                        + '%0A%0A'
+                        + '[' + t + '][' + idRef + ']:'
+                        + '%0A%0A'
+                        + '>%20' + selectedText
+                        + '%0A%0A'
+                        + mdLink
+
